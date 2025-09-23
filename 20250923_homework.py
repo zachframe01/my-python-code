e36f2b828d744dd1e3eeae5e7f0d6c70f23b0052
zachframe01 <zachframe01@gmail.com>
2025-09-23 14:49:29 -0700

    Add new script

---
+++ 20250923_homework.py
@@ -0,0 +1,24 @@
+'''
+#color_name with a width of 12 characters,
+#  left-aligned, and with the fill character "="
+#the percent sign, "%"
+#color_intensity with a width of 
+# 12 characters, centered, and with 
+# the fill character "="
+
+
+
+item_name = input("type something:")
+
+
+print(f"{item_name:+^12}")
+print(f"{item_name:+>12}")'''
+
+#Floating-point number air_temperature is read from input.
+#  Use one print(f" ") statement to output "Temperature:
+#  ", followed by air_temperature to 4 decimal places 
+# and "C".
+
+air_temperature = float(input("enter air temp:"))
+
+print(f"Temperature: {air_temperature:.4f} C")
