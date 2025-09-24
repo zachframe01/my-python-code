2a98c8ee2c9b07b6491029e5e48e2b2e28337010
zachframe01 <zachframe01@gmail.com>
2025-09-24 14:37:56 -0700

    Add new script

--- 20250923_homework.py
+++ 20250923_homework.py
@@ -1,4 +1,6 @@
-'''
+''' 
+#learning: Advanced String Formatting
+
 #color_name with a width of 12 characters,
 #  left-aligned, and with the fill character "="
 #the percent sign, "%"
@@ -12,7 +14,10 @@ item_name = input("type something:")
 
 
 print(f"{item_name:+^12}")
-print(f"{item_name:+>12}")'''
+print(f"{item_name:+>12}")
+
+#new assignment
+
 
 #Floating-point number air_temperature is read from input.
 #  Use one print(f" ") statement to output "Temperature:
@@ -21,4 +26,11 @@ print(f"{item_name:+>12}")'''
 
 air_temperature = float(input("enter air temp:"))
 
-print(f"Temperature: {air_temperature:.4f} C")
+print(f"Temperature: {air_temperature:.4f}C")
+'''
+
+#stripping whitespace
+
+text = "   / hello world /   "
+print(text)
+print(text.strip())
