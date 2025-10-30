737511515a078e7f7370213c73f85b53fe5d0131
zachframe01 <zachframe01@gmail.com>
2025-10-30 16:32:32 -0700

    snake game

--- Snake_Game.py
+++ Snake_Game.py
@@ -64,6 +64,8 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
+
 
 
 
@@ -89,13 +91,31 @@ def move():
 #main game loop
 while True:
     window.update()
-
+#check for collision with food
     if head.distance(food) < 20:
         #move the food to a random spot
         x = random.randint(-290,290)
         y = random.randint(-290,290)
         food.goto(x,y)
+        # add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.color("grey")
+        new_segment.shape("square")
+        new_segment.penup()
+        segments.append(new_segment)
 
+    #move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+        #move segment 0 to where the head is 
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
     move()
 
     time.sleep(delay)
--- typecasting.py
+++ typecasting.py
@@ -3,4 +3,6 @@ print(f"wow! my favorite food is also {food}!")
 
 print("hello guy.", food, 5, 10, 20, sep="/")
 
-
+a = 34
+b = 2
+print("a+b = ", a + b)
